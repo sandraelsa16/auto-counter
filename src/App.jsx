48a802a9eee20566ff8bc330c6bf5a0aaebc73f9
sandraelsa16 <sandraelsavarughese@gmail.com>
2024-08-09 01:42:34 +0530

    feat:added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
- 
+
+ const[counter,setCounter]=useState(0)
+ const [isRunning, setIsRunning] = useState(true);
+
+ useEffect(() => {
+  if (isRunning) {
+      const intervalId = setInterval(() => {
+          setCounter(prevCounter => prevCounter + 1);
+      }, 1000);
+
+      return () => clearInterval(intervalId);
+  }
+}, [isRunning]);
+
+// Stop the counter
+const stopCounter = () => {
+  setIsRunning(prevIsRunning => !prevIsRunning);
+};
+
+// Reset the counter and restart
+const resetCounter = () => {
+  setCounter(0);
+  setIsRunning(true);
+};
+
   return (
     <>
-      
+      <div className='container'>
+        <div className="counter-part">
+            <p>{counter}</p>
+            <div>
+              <button onClick={stopCounter} >Pause</button>
+              <button onClick={resetCounter}>Reset</button>
+            </div>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+function Header() {
+  return (
+    <div className='nav'>
+      <h1>Auto Counter</h1>
+    </div>
+  )
+}
+
+export default Header
